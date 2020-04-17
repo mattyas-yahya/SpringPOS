e58956f5f286f9c6c00106c730207f1061a3180d
theJCrakcer <rahmadandmysella@gmail.com>
2020-04-18 02:07:13 +0700

    Update spring_rest.js

--- src/main/resources/static/dist/js/spring_rest.js
+++ src/main/resources/static/dist/js/spring_rest.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
       {
           $.ajax({
-               url: 'http://localhost:8081/BarangAll',//http://localhost:1111/BarangAll
+               url: 'https://squarepos.herokuapp.com/BarangAll',//http://localhost:1111/BarangAll
                success: function(data)
                 {
                  data.forEach(function(dt)
